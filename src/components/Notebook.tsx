870f50da08546492aba082233dd3f1b341e1293b
David Chen <dvychen@gmail.com>
2023-09-26 01:57:47 -0400

    add some points about myself, add highlight

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -1,16 +1,31 @@
 import { FC } from 'react';
 import { Page } from './Page';
 import '../styles/Notebook.css';
+import '../styles/Highlight.css';
 
 export const Notebook: FC = () => {
 	return (
 		<div className='notebook'>
 			<Page>
 				<div className='header1'>DAVID CHEN</div>
-				<div>Hi, I'm David!</div>
+				<div className=''>Hi, I'm David!</div>
+				<ul>
+					<li>
+						Computer Science @ University of Waterloo (graduating
+						<span className='highlight'>April 2025</span>)
+					</li>
+					<li>
+						Working as SWE @ Transify - I'm the very first employee!
+					</li>
+					<li>
+						Researching @ VIP Lab - 3D avatar reconstruction from
+						videos via neural nets
+					</li>
+				</ul>
 			</Page>
 			<Page>
-				<div>Previously done a lot of things</div>
+				<div>Previously done a lot of things... </div>
+				<div style={{ textAlign: 'center' }}>(coming soon...)</div>
 			</Page>
 		</div>
 	);
