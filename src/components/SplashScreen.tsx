d2c6ff9fd8558749564a2fb8cc1e1b69a1d47ca7
David Chen <dvychen@gmail.com>
2023-09-28 02:54:32 -0400

    make splashscreen fade-out

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,17 +1,23 @@
 import { FC, useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import AnimatedDavidChenWriting from '../assets/animated-davidchen-writing-transparent.gif';
 import '../styles/SplashScreen.css';
 
 const SplashScreen: FC = () => {
 	return (
-		<div className='splash-screen'>
+		<motion.div
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 1.5 }}
+			className='splash-screen'
+		>
 			<img
 				src={AnimatedDavidChenWriting}
 				alt='Animated writing of "David Chen"'
 				className='animated-drawing'
 			/>
-		</div>
+		</motion.div>
 	);
 };
 
@@ -20,7 +26,7 @@ export const withSplashScreen = (Component: React.FC) => {
 		const [loading, setLoading] = useState<boolean>(true);
 
 		useEffect(() => {
-			const timeForSplashScreen = 2500;
+			const timeForSplashScreen = 3000;
 			setTimeout(() => {
 				setLoading(false);
 			}, timeForSplashScreen);
@@ -28,7 +34,7 @@ export const withSplashScreen = (Component: React.FC) => {
 
 		return (
 			<>
-				{loading && <SplashScreen />}
+				<AnimatePresence>{loading && <SplashScreen />}</AnimatePresence>
 				<Component {...props} />
 			</>
 		);
