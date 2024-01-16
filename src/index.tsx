c6f14e62cad1dbde1468f2151e6fa97587e9e593
David Chen <dvychen@gmail.com>
2024-01-16 18:48:24 -0500

    Add GA4, run npm audit fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactGA from 'react-ga4';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Google Analytics
+ReactGA.initialize('G-CZYSM5GVS4');
